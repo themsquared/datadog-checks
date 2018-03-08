bcb97685dd7088b174ecf9044cb37f6bd4d8b57a
Mike Moore <mike@themsquared.com>
2018-03-08 11:17:06 -0500

    File Check
    Custom file exists check

---
+++ file/README.md
@@ -0,0 +1,3 @@
+# File Check
+
+A quick Datadog Agent check to determine if a file exists and for how long it has existed.
---
+++ file/filecheck.py
@@ -0,0 +1,34 @@
+import time
+import os
+
+from checks import AgentCheck
+
+class FileCheck(AgentCheck):
+    def check(self, instance):
+        if 'path' not in instance:
+            self.log.info("Skipping instance, no path found.")
+            return
+
+        path = instance['path']
+        result = AgentCheck.CRITICAL
+        message = "File "+path+" was not found.";
+        if os.path.isfile(path):
+            result = AgentCheck.OK
+            message = "File "+path+" was found."
+            
+            st=os.stat(path)    
+            mtime=st.st_mtime
+            age=time.time()-mtime
+            self.gauge('file.modified',mtime,["path"+path])
+            self.gauge('file.age',age,["path"+path])
+        
+        self.service_check('file.exists',result,tags=["path"+path],message=message)
+
+if __name__ == '__main__':
+    check, instances = HTTPCheck.from_yaml('/etc/datadog-agent/conf.d/http.yaml')
+    for instance in instances:
+        print "\nRunning the check against url: %s" % (instance['url'])
+        check.check(instance)
+        if check.has_events():
+            print 'Events: %s' % (check.get_events())
+        print 'Metrics: %s' % (check.get_metrics())
